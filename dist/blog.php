25fcf10acf3955c01d0e573e57a8745608322ece
Denis Scholokov <scholokov@users.noreply.github.com>
2021-05-24 16:34:12 +0300

    testing images

--- dist/blog.php
+++ dist/blog.php
@@ -18,6 +18,12 @@
 
     <!-- <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet"> -->
 
+    <!-- jQuery 1.8 or later, 33 KB -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+    <!-- Fotorama from CDNJS, 19 KB -->
+    <link  href="https://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.css" rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fotorama/4.6.4/fotorama.js"></script>
 
     <!-- Document Title
 	============================================= -->
@@ -138,6 +144,16 @@
                             include_once $foldername . '/' . $result["descriptionarticle"] . '.html' ; 
                             echo '</h4>'    ;
                         }
+
+
+                        echo '<div class="fotorama">' ;
+                            echo   '<img src="https://s.fotorama.io/1.jpg">' ;
+                            echo '<img src="https://s.fotorama.io/2.jpg">' ;
+                        echo '</div>' ;
+
+
+
+
 
                         echo    '<div class=photo-galery>' ;
                         if ( $result["foto"] ){
