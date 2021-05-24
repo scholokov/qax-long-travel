6098ed9bc56bb11a19082789a189a97d44e1d8fb
Denis Scholokov <scholokov@users.noreply.github.com>
2021-05-24 17:12:26 +0300

    photo gallery

--- dist/blog.php
+++ dist/blog.php
@@ -149,16 +149,16 @@
                             echo '</div>';
                             
                             
-                            echo    '<div class=photo-galery>' ;
                             if ( $result["foto"] ){
-                                echo        '<div class="fotorama" data-nav="thumbs" data-allowfullscreen="native" data-keyboard="true" data-height="100%" data-width="100%" data-max-height="820" data-navposition="top">';
+                                echo    '<div class=photo-galery>' ;
+                                echo    '<div class="fotorama" data-nav="thumbs" data-allowfullscreen="native" data-keyboard="true" data-height="100%" data-width="100%" data-max-height="820" data-navposition="top">';
                                 
-                                            $files = scandir( $full_folder_name . '/images/' . $result["foto"] . '/' );
-                                            foreach($files as $file) {
-                                                if ( $file != '.' AND $file != '..' )
-                                                    echo '<img src="' . $full_folder_name . '/images/' . $result["foto"] . '/' . $file . '">' ;
+                                        $files = scandir( $full_folder_name . '/images/' . $result["foto"] . '/' );
+                                        foreach($files as $file) {
+                                            if ( $file != '.' AND $file != '..' )
+                                                echo '<img src="' . $full_folder_name . '/images/' . $result["foto"] . '/' . $file . '">' ;
                                             }
-                                echo        '</div>';
+                                echo '</div>';
                             }
                             
                         };
